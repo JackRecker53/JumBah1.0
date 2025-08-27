b7d4ff76b061b2b66e5b3b56ddbe09e83a20d1b3
Hafidzirfan <mohdhafidzirfan95@gmail.com>
2025-08-27 14:50:36 +0800

    chore: remove stale code from events page

--- Frontend/src/pages/EventsPage.jsx
+++ Frontend/src/pages/EventsPage.jsx
@@ -1,33 +1,3 @@
-// import React from "react";
-// import { upcomingEvents } from "../data/events";
-// import "../styles/EventsPage.css";
-
-// const EventsPage = () => {
-//   return (
-//     <div className="container eventsPage">
-//       <h1>Current & Upcoming Events</h1>
-//       <p>
-//         Immerse yourself in the vibrant culture of Sabah. Here's what's
-//         happening!
-//       </p>
-//       <div className="eventList">
-//         {upcomingEvents.map((event) => (
-//           <div key={event.id} className="eventCard">
-//             <div className="eventInfo">
-//               <span className="eventDate">{event.date}</span>
-//               <h2 className="eventTitle">{event.title}</h2>
-//               <p className="eventLocation">{event.location}</p>
-//               <p className="eventDescription">{event.description}</p>
-//             </div>
-//           </div>
-//         ))}
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default EventsPage;
-
 import React from "react";
 import { Link } from "react-router-dom";
 import { upcomingEvents } from "../data/events";
